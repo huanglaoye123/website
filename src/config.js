603f85d5670fb6654736b9a2e5a1b46abc4e6f73
huangqing@madecare.com <huangqing@madecare.com>
2021-06-22 16:47:18 +0800

    no message

---
+++ src/config.js
@@ -0,0 +1,260 @@
+import React from 'react';
+
+
+
+export const NEWS_DETAIL = {
+	news6:{
+	  title:'有效逆转高血压的“新处方”',
+	  time:'2020-11-13',
+	  publisher:'xxx',
+	  items:[      
+	    {txt:'2019年中国心血管健康与疾病报告最新统计，大于18岁的成年人高血压患病率27.9%。我国成年人中每4个人就有1个高血压患者，是影响我国居民心血管健康的头号健康大敌。'},
+		{txt:'在2020年5月国际高血压学会制定了世界范围内使用的最新版ISH2020国际高血压实践指南，正式发布。'},
+		{txt:'《ISH2020国际高血压实践指南》中指出：改善生活方式已经成为第一线的降压治疗手段。'},	
+	    {src:'https://wx1.sinaimg.cn/mw690/0080kruWly1gpi2s585z4g30df07l7wp.gif'},
+	    {txt:'来自北京大学人民医院心血管内科的丁荣晶主任给我们讲讲有效逆转高血压的“新处方”。'},
+	    {src:'https://wx1.sinaimg.cn/mw690/0080kruWly1gpi2u0jattg30en08ahe0.gif'},
+	    {txt:'丁荣晶   副主任医师 北京大学人民医院 心血管内科'},
+		{src:require('./assets/bigimg/new6-3.jpg')},
+		{src:'https://wx3.sinaimg.cn/mw690/0080kruWly1gpi2u6pq5tg30hr0a17wp.gif'},
+		{txt:'“新处方”多久有效果'},
+		{txt:'“降压的“新处方”维持3-6个月，一次的治疗后，一个小时左右，实现5-10毫米汞柱的高压下降，3-5毫米汞柱的低压下降 。根据患者情况会让药物减掉或者不用吃药。'},
+		{src:'https://wx4.sinaimg.cn/mw690/0080kruWly1gpi2ubucmpg30f508ne89.gif'},
+		{txt:'“定制“新处方”——心肺运动负荷试验'},
+		{txt:'“高血压患者血压：高压：147mmhg   低压：109mmhg'},
+		{src:'https://wx1.sinaimg.cn/mw690/0080kruWly1gpi2ugjpftg30b606dx6w.gif'},
+		{src:'https://wx1.sinaimg.cn/mw690/0080kruWly1gpi2ulh9owg30hr0a5u10.gif'},
+		{txt:'“开“新处方”前做一个重要的测试——心肺运动负荷试验。'},
+		{txt:'观察患者的活动状态以及血压、心率、心血管的反映情况，根据患者的反映情况量身定制“新处方”，帮助患者在日常生活中会降压。'},
+		{src:'https://wx1.sinaimg.cn/mw690/0080kruWly1gpi2vsgbsgg30cw07dnpk.gif'},
+		{txt:'心肺运动负荷试验是一项重要的检查。'},
+		{txt:'日常生活中，人们在不了解自己的情况下活动时，风险更大。进行心肺运动负荷试验后，了解自己身体能接受的强度，在日常生活中是可控心血管的风险。'},
+		{src:'https://wx4.sinaimg.cn/mw690/0080kruWly1gpi2vx6ly7g30c406xqvc.gif'},
+		{src:require('./assets/bigimg/new6-10.png')},
+		{txt:'"配有遥测运动心电及血压的高度自动化心肺评测系统（CPX）。"'},
+		{txt:'主要适用于心内科、呼吸科、康复科等需要进行心肺功能测定和康复的患者，以及各种需要心肺功能评测指导下的身体康复的患者。'},
+		{title:'产品功能'},
+		{txt:'流速、容量测试'},{txt:"一口气法气体成分分析（VO2， VCO2）"},{txt:"静态/动态肺功能测试"},{txt:"静态/运动心电图测量及分析"},
+		{txt:"基础/营养代谢测定及分析"},{txt:"运动血压测量和监控"},{txt:"血氧饱和度测量和监控"},{txt:"无创心排量测量（Wasserman）"},
+		{txt:"辅助分析流程"},{txt:"ACSM运动处方"},{txt:"多维度数据导出"},
+		{title:'产品优势'},
+		{txt:'精准测量、高度可靠{快反应气体成分分析 、专利设计流量传感器}'},{txt:"易学易用{多维导航、一键自动定标、智能辅助 }"},{txt:"全周期（早、中、后期）适用"},{txt:"无线分立的数据采集"},
+		{txt:"紧凑型、可穿戴式设计"},
+		{title:'满足发布的指导要求'},
+		{txt:'国际权威的美国心脏协会（AHA）'},
+		{txt:'美国胸科协会（ATS）'},{txt:"美国胸科医师协会（ACCP）"},{txt:"美国运动医学会（ACSM）"},
+		{txt:'高血压患者进行心肺运动负荷试验后：'},
+		{txt:'高压：190mmhg   低压：114mmhg 心率：115次每分  呼吸急促、微汗'},
+		{src:'https://wx1.sinaimg.cn/mw690/0080kruWly1gpi2w4qaqkg30ch074qvc.gif'},
+		{txt:'打印报告，量身定制“降压运动处方”'},
+		{src:'https://wx1.sinaimg.cn/mw690/0080kruWly1gpi2wax9r9g30c206wnpk.gif'},
+		{title:'解读高血压患者测试报告'},
+		{txt:'患者达到了最大运动耐力，在达到最高运动强度时，血压：190mmhg。'},
+		{txt:'患者的运动状态血压应激反应特别明显。	'},
+		{txt:'没有高血压的个体在运动中，血压上升，意味着有患高血压的高度风险。'},
+		{src:'https://wx3.sinaimg.cn/mw690/0080kruWly1gpi2wffur5g30d607jb2g.gif'},
+		{title:'量身定制“降压新处方”'},
+		{txt:'根据高血压患者个人身体情况定制：'},
+		{txt:'有氧运动、蹬车交替，每天30分钟'},
+		{txt:'心率90-100分/次 每周7天'},
+		{src:'https://wx1.sinaimg.cn/mw690/0080kruWly1gpi2xgqhl2g30am0627wo.gif'},
+		{src:'https://wx4.sinaimg.cn/mw690/0080kruWly1gpi2xpfmx0g30b806f7wp.gif'},
+		{src:'https://wx1.sinaimg.cn/mw690/0080kruWly1gpi2xus2h5g30dz07znpk.gif'},
+		{src:'https://wx2.sinaimg.cn/mw690/0080kruWly1gpi2xz3lc2g30hr0a5hdy.gif'},
+		{iframe :'https://v.qq.com/txp/iframe/player.html?vid=w3203sjyma4'},
+		
+	  ]
+	},
+  news5:{
+    title:'美德智能康复护理医疗器械产品亮相第二届“北京国际医学工程大会暨产品与技术交易博览会”',
+    time:'2017-10-15',
+    publisher:'xxx',
+    items:[
+      {txt:'为推动医学工程发展，提升医学创新能力，2017年10月13至14日，第二届北京国际医学工程大会暨产品与技术交易博览会在中关村医学工程转化中心园区隆重举行。'},
+      {txt:'美德远健展出了全新的有自主专利的创新产品，领动TM系列智能康复解决方案及致护TM系列智能护理解决方案。智能转移和康复系统、智能导尿监护和膀胱康复系统以及床旁长期监护系统的亮相，引来了与会专家和观众的广泛关注和高度评价，在展会上独树一帜。智能转移和康复系统有效解决了病人的减重下肢康复训练和全覆盖的安全转移问题，智能导尿系统有效解决了中国每年3000多万套留置式导尿管的使用者的膀胱保护和康复、日夜不间断的护理负担，长期床旁监护以一种新的技术让病人和老人的健康监测、安全照护和疾病预警变得简单和智能。'},
+      {src:require('./assets/imgs/News/news3.jpg')},
+      {txt:'本届大会由中国食品药品企业质量安全促进会和中关村医学工程转化中心共同主办，中关村国际医药检验认证科技有限公司协办。参加本次会议的有中、加、英、美、以色列等国家近40名医学工程研究领域的专家学者，其中不乏在国际医学舞台上被公认的顶级领军人物。国家食药监总局副局长孙咸泽、国家中医药管理局副局长于文明以及工信部、卫计委、科技部火炬中心、北京市和海淀区政府部门相关领导也出席了本次大会。'}
+    ]
+  },
+  news4:{
+    title:'美德远健在2018春季CMEF初次亮相并获得广泛关注',
+    time:'2018-04-11',
+    publisher:'xxx',
+    items:[
+      {txt:`2018年4月11-14日，美德远健精彩亮相亚太地区最大的医疗器械展览会-中国国际医疗器械博览会（CMEF），吸引了海内外客户、友商以及代理商的高度关注。
+        美德远健秉持“数据引领 智能康护”的理念，首次亮相就推出了多款数字化、智能化的高端康复/护理产品，包括力感减重下肢康复机器人、智能转移/康复系统、长期
+        床旁监护系统，几款产品均获得了终端用户的广泛好评。`},
+      {src:require('./assets/imgs/News/news2.jpg')},
+      {src:require('./assets/imgs/News/news2-1.jpg')},
+      {src:require('./assets/bigimg/news2-2.jpg')},
+      {src:require('./assets/imgs/News/news2-3.jpg')}
+    ]
+  },
+  news3:{
+    title:'美德远健携多款高端康复医疗设备成功签约入驻石家庄高新区',
+    time:'2018-05-18',
+    publisher:'xxx',
+    items:[
+      {src:require('./assets/bigimg/news1.jpg')},
+      {txt:'石家庄（北京）生物医药健康产业专题对接会签约仪式于2018年5月16日在北京圆满完成，美德远健作为重点邀请入区企业成功签约入区。'},
+      {txt:'美德远健作为国家级高新技术企业，携多款旗下高端康复医疗设备入驻石家庄高新区，将助力石家庄作为国家康复辅助器具产业综合创新试点城市迈向更高的台阶。'}
+    ]
+  },
+  news2:{
+    title:'展会回顾|“无感”监护 第八届中国国际养老服务业博览会',
+    time:'2019-08-28',
+    publisher:'xxx',
+    items:[      
+      {txt:'石家庄（北京）生物医药健康产业专题对接会签约仪式于2018年5月16日在北京圆满完成，美德远健作为重点邀请入区企业成功签约入区。'},
+      {src:require('./assets/bigimg/828_1.gif')},
+      {txt:'美德远健以“无感”监护为主题，以非接触式为核心，借技术变革与重构之际洞见养老新机遇，打开智能养老未来之门。'},
+      {src:require('./assets/bigimg/828_2.png')},
+      {txt:'此次美德远健携致护™长期床旁监护系统 PMS-600 隆重登场。吸引了众多业内人士和观展商驻足观摩，受到了众多专家学者们的一致好评。'},
+      {src:require('./assets/bigimg/828_3.png')},
+      // {src:require('./assets/imgs/News/2019/828_4.png')},
+      {src:require('./assets/imgs/News/2019/828_5.png')},
+      {txt:'“PMS-600长期床旁监护系统”是一款适合于养老院、医院等，用于老人或患者长期健康监测和身体异常预警的系统。采用了先进的传感技术及算法，能对心率、呼吸、体动进行监测，实现了心率变异分析、翻身提醒、睡眠分析、中央监测等功能。完全不接触身体、不干扰使用者的生活习惯，基于云的数据采集和管理技术将助力远程监护和中央监测先进管理模式的升级。'},
+      // {src:require('./assets/imgs/News/2019/828_6.gif')},
+      // {src:require('./assets/imgs/News/2019/828_7.png')},
+      // {src:require('./assets/imgs/News/2019/828_8.png')},
+      // {src:require('./assets/imgs/News/2019/828_9.gif')}
+    ]
+  },
+  news1:{
+    title:'化繁为简|国际康复设备展览会',
+    time:'2019-11-8',
+    publisher:'xxx',
+    items:[
+      {src:require('./assets/imgs/News/118.png')},
+      {txt:'为加强康复医学学术交流与合作，发挥高端学术会议引领辐射作用，打造综合学术年会品牌，2019年11月22至24日，中国康复医学会将在北京市国家会议中心举办2019第三届中国康复医学会综合学术年会暨国际康复设备展览会。'},
+      {txt:'本届会议围绕“守正创新，融合发展”主题，紧盯康复医学科技发展前沿，安排第三届国际综合康复主论坛及50余场学术会议，会聚康复、医疗、护理、疗养、养老等领域国内外知名专家，研讨交流先进康复理念、技术创新与服务经验，展示国内外康复医学科技发展最新成果，开展康复技术理论和操作培训，谋划助推康复医学未来发展，汇集诸多实用与前沿，国内与国际兼容并蓄精品内容。'},
+      {txt:'此次大会，美德远健将携能将CPET测试“化繁为简”的CPX-600高级心肺功能评测系统隆重登场，诚挚地邀请您莅临美德远健展位C45参观体验！'},
+      {src:'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/a8d82563-7327-4281-a695-6a550d609d9c.png'},
+      {txt:'“CPX-600高级心肺功能评测系统”是一款采用先进传感器技术、先进算法，集运动气体代谢、运动心电/血压/血氧监测和营养代谢评估于一体，具有全面心肺功能快速精确评测的医疗设备。满足国际权威的美国心脏协会（AHA）、美国胸科协会（ATS）、美国胸科医师协会（ACCP）和美国运动医学会（ACSM）等发布的指导要求。适用于康复科、心脏科、呼吸科等需要进行心肺功能测定和康复的患者，以及各种需要心肺功能评测指导下的身体康复的患者。主要针对心肺功能不全者进行运动心肺试验、静态心测试、静态肺功能测试，对营养代谢疾病患者进行运动营养代谢分析、基础代谢率测定。'},
+      // {src:require('./assets/imgs/News/640.gif')},
+      {src:require('./assets/imgs/News/2019/7.jpg')}
+    ]
+  },
+  news0:{
+    title:'了解心肺功能——CPET“肺”他莫属',
+    time:'2020-10-29',
+    publisher:'xxx',
+    items:[
+      {txt:'随着社会的高度发展，当前威胁我们健康的主要原因已由传染类疾病过渡到以心血管病、“ 三高”为代表的慢性病。而引起这些慢性病的主要原因之一就是缺乏足够的运动。因此，越来越多的人认识到运动的重要性，也积极主动地参与到运动健身的大潮中去。但运动隐藏着一定的风险。'},
+      {txt:'运动的好处: 通过科学的运动锻炼有助于降低血脂，可以提高心肺适应能力，增加心肌功能，减少血小板聚集；延缓动脉粥样硬化斑块的形成。'},
+      {txt:'运动的风险: 我们必须要明确运动是一柄双刃剑，在它在帮助我们改善健康、提高身体素质的同时也可能会导致如猝死、晕厥、损伤等危险事件发生。在运动所引发的所有风险中，心血管风险的危害无疑是最为严重的。根据调查发现，随着年龄的增长，人们从20 ~ 69 岁在运动中发生胸闷、胸痛、呼吸困难等心血管风险的比例由4 % 上升至58%，可谓是增长迅速，来势汹汹。'},
+      {txt:'自测VS精准测评：日常生活中，我们自测运动前后的脉搏差、血压等一些方法都不够精准和全面分析运动中心血管的风险。'},
+      {txt:'进行精准的测评，需要做CPET心肺运动负荷试验，不仅可以帮助大家及时了解自己的心血管情况，及时发现心血管风险，还可以及时采取防范措施，避免风险恶化而导致猝死等严重意外发生。'},     
+      {txt:'下面我们来看CCTV2《职场健康课》节目中邀请北京大学第三医院心脏中心主任医师徐顺霖，是如何评估心肺耐力——CPET心肺运动负荷试验。'},
+      {src:'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/ace3f633-f5b4-4cb3-9a49-1490fb3e72f3.gif'},           
+      {txt:'评估心肺耐力需要做心肺运动负荷试验'},
+      {src:'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/cfa037a3-365c-4484-b166-c389c71fbd86.gif'},
+      {txt:'测试者情况：马拉松选手——跑过多次马拉松，每次结束后很疲惫'},     
+      {src:'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/b373ad1c-0848-4ebf-b36a-40eed632cdd3.gif'},
+      {txt:'逐步增加测试者的阻力'},     
+      {txt:'下面来看心血管的反应'},     
+      {src:'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/61b0e7d7-85ed-4e8f-980b-a2cf87502b7b.gif'},     
+      {txt:'红色的线：摄氧量'},     
+      {txt:'蓝色的线：二氧化碳'},     
+      {txt:'均为达标'},     
+      {src:'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/03586c72-c2de-4be3-98aa-ee497c4bdd41.gif'},     
+      {txt:'运动到极限的时候，发生了多次早搏'},     
+      {txt:'从运动实验的角度，不安全'},     
+      {txt:'这样的情况心脏科医生禁止跑马拉松'},     
+      {src:''},     
+      {txt:'在运动中出现的心血管风险如果不能引起大家的高度重视，那么我们为了获得健康而进行的运动就会变得不安全。为了让我们更有效，更安全的运动，我们应该在运动前提前了解自己在运动中出现在的问题，从而防范风险的发生。'},     
+      {src:'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/ba0bf75b-ac3d-447d-8f42-491f7daf9b96.gif'},     
+      {txt:'运动中风险的防范'},     
+      {txt:'1.做心肺运动负荷试验，进行精准评估和全面分析心肺耐力。'},     
+      {txt:'2.通过心肺运动负荷试验的评估报告，选择适合自己的运动形式。'},     
+      {txt:'3.合理增加运动频率。'},     
+      {txt:'4.注意运动强度。'},     
+      {video:'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/a326f4d9-8bc7-41b9-9caa-ef73285339cc.mp4'}
+    ]
+  }
+};
+
+export const NEWS_ABSTRACT = [
+  {title:'有效逆转高血压的“新处方”',time:'2020-08-28',cnt:'2019年中国心血管健康与疾病报告最新统计，大于18岁的成年人高血压患病率27.9%。我国成年人中每4个人就有1个高血压患者，是影响我国居民心血管健康的头号健康大敌...',src:require('./assets/bigimg/new6-1.gif'),to:'/news/newsdetail#news6'},
+  {title:'了解心肺功能——CPET“肺”他莫属',time:'2020-10-29',cnt:'随着社会的高度发展，当前威胁我们健康的主要原因已由传染类疾病过渡到以心血管病、“ 三高”为代表的慢性病。而引起这些慢性病的主要原因之...',src:'https://ftp.bmp.ovh/imgs/2020/11/dcb1202779b07b87.gif',to:'/news/newsdetail#news0'},
+  {title:'化繁为简|国际康复设备展览会',time:'2019-11-8',cnt:'为加强康复医学学术交流与合作，发挥高端学术会议引领辐射作用，打造综合学术年会品牌，2019年11月22至24日，中国康复医学会将...',src:require('./assets/imgs/News/118.png'),to:'/news/newsdetail#news1'},
+  {title:'展会回顾|“无感”监护 第八届中国国际养老服务业博览会',time:'2019-8-28',cnt:'第八届中国国际养老服务业博览会吸引了来自20多个国家和地区的220多家社会组织、机构及企业参展；本届博览会创新办会模式，由以往...',src:require('./assets/imgs/News/828.png'),to:'/news/newsdetail#news2'},
+  {title:'美德远健携多款高端康复医疗设备成功签约入驻石家庄高新区',cnt:'石家庄（北京）生物医药健康产业专题对接会签约仪式于2018年5月16日在北京圆满完成，美德远健作为重点邀请入区企业成功签约入区...',time:'2018-05-18',src:require('./assets/bigimg/news1.jpg'),to:'/news/newsdetail#news3'},
+  {title:'美德远健在2018春季CMEF初次亮相并获得广泛关注',time:'2018-04-11',cnt:'2018年4月11-14日，美德远健精彩亮相亚太地区最大的医疗器械展览会-中国国际医疗器械博览会（CMEF），吸引了海内外客户、友商以及代理商的高度关注...',src:require('./assets/imgs/News/news2.jpg'),to:'/news/newsdetail#news4'},
+  {title:'美德智能康复护理医疗器械产品亮相第二届“北京国际医学工程大会暨产品与技术交易博览会”',cnt:'为推动医学工程发展，提升医学创新能力，2017年10月13至14日，第二届北京国际医学工程大会暨产品与技术交易博览会在中关村医学工程转化中心园区隆重举行...',time:'2017-10-15',src:require('./assets/imgs/News/news3.jpg'),to:'/news/newsdetail#news5'}
+];
+
+export const NAVS = [
+  // {txt:'nav_product',to:'/product',icon:'shopping_cart',items:[{txt:'nav_product_dws1000',to:'/product/dws1000',src:require('./assets/imgs/NavHeader/prod5.png')},{txt:'nav_product_tr800',to:'/product/tr800',src:require('./assets/imgs/NavHeader/prod6.png')},{txt:'nav_product_tr600',to:'/product/tr600',src:require('./assets/imgs/NavHeader/prod1.png')},{txt:'nav_product_pms600',to:'/product/pms600',src:require('./assets/imgs/NavHeader/prod2.png')},{txt:'nav_product_sc600',to:'/product/sc600',src:require('./assets/imgs/NavHeader/prod3.png')},{txt:'nav_product_cloud',to:'/product/cloud',src:require('./assets/imgs/NavHeader/prod4.png')}]},
+  {txt:'nav_solution',to:'/solution',icon:'assignment_ind',items:[{txt:'nav_solution_rehab',to:'/solution/recovery'},{txt:'nav_solution_transfer',to:'/solution/transfer'},{txt:'nav_solution_protection',to:'/solution/prevent'},{txt:'nav_solution_monitor',to:'/solution/monitor'},{txt:'nav_solution_manage',to:'/solution/remote'},{txt:'nav_solution_cpx',to:'/solution/cpx'},]},
+  {txt:'nav_about',to:'/about',icon:'supervisor_account',items:[{txt:'nav_about_profile',to:'/about'},{txt:'nav_about_team',to:'/about'},{txt:'nav_about_vision',to:'/about'},{txt:'nav_about_mission',to:'/about'},{txt:'nav_about_culture',to:'/about'},]},
+  {txt:'nav_new',icon:'fiber_new',to:'/news'},
+  {txt:'nav_job',icon:'work',to:'/recruit'},
+  {txt:'nav_contact',icon:'room',to:'/contact'},
+];
+
+export const HEADER_NAV = [
+  {txt:'nav_home',to:'/',icon:'home'},
+  ...NAVS
+];
+
+export const LANGS = ['zh','en'];
+export const DEF_LANG = 'zh';
+
+
+export const ROUTE_OBJ = {
+  recovery: '减重下肢康复',
+  solution: '解决方案',
+  transfer: '患者安全转移',
+  prevent: '膀胱保护和康复',
+  cpx: '高级心肺评测系统',
+  remote: '远程云管理',
+  monitor: '长期床旁监护',
+  product: '产品中心',
+  cloud: '软件云平台',
+  about: '关于我们',
+  recruit: '远健招聘',
+  jobdetail: '职位详情',
+  tr800: '全自动智能转移系统',
+  tr600: '智能康复/转移系统',
+  pms600: '长期床旁监护系统',
+  sc600: '智能导尿器/智能导尿监护系统',
+  dws1000: <span>
+    <span style={{ color: '#f69421' }}>力感减重</span><sup style={{
+    fontSize: '10px',
+    fontWeight: 'normal',
+    top: '-2em',
+  }}>TM</sup>下肢康复机器人</span>,
+  contact: '联系我们',
+  news:'远健动态',
+  newsdetail:'新闻详情'
+};
+
+
+export const ROUTE_OBJ_EN = {
+  recovery: 'Limbs Rehabilitation',
+  solution: 'Solutions',
+  transfer: 'Patient Safe Transfer',
+  prevent: 'Urinary Catheterization',
+  cpx: 'Cardiopulmonary Test & Rehabilitation',
+  remote: 'Tele-Rehabilitation',
+  monitor: 'Continuous Patient Monitoring',
+  product: '产品中心',
+  cloud: '软件云平台',
+  about: 'About',
+  recruit: '远健招聘',
+  jobdetail: '职位详情',
+  tr800: '全自动智能转移系统',
+  tr600: '智能康复/转移系统',
+  pms600: '长期床旁监护系统',
+  sc600: '智能导尿器/智能导尿监护系统',
+  dws1000: <span>
+    <span style={{ color: '#f69421' }}>力感减重</span><sup style={{
+    fontSize: '10px',
+    fontWeight: 'normal',
+    top: '-2em',
+  }}>TM</sup>下肢康复机器人</span>,
+  contact: 'Contact',
+  news:'News',
+  newsdetail:'新闻详情'
+};
